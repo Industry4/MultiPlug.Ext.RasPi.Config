b986f4cb09a6d3c091a9cba30d9dcb9584c03e6d
David Graham <hello@industry4.uk>
2025-02-10 15:36:15 +0000

    #18

--- src/MultiPlug.Ext.RasPi.Config/Components/Performance/PerformanceComponent.cs
+++ src/MultiPlug.Ext.RasPi.Config/Components/Performance/PerformanceComponent.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using MultiPlug.Ext.RasPi.Config.Models.Components.Performance;
 using MultiPlug.Ext.RasPi.Config.Utils.Swan;
 using MultiPlug.Ext.RasPi.Config.Diagnostics;
@@ -9,20 +10,96 @@ namespace MultiPlug.Ext.RasPi.Config.Components.Performance
     {
         internal event Action<Diagnostics.EventLogEntryCodes, string[]> Log;
 
+        private const int c_UndervoltageDetected = 0x1;
+        private const int c_ArmFrequencyCapped = 0x2;
+        private const int c_CurrentlyThrottled = 0x4;
+        private const int c_SoftTemperatureLimitActive = 0x8;
+        private const int c_UndervoltageHasOccurred = 0x10000;
+        private const int c_ArmFrequencyCappingHasOccurred = 0x20000;
+        private const int c_ThrottlingHasOccurred = 0x40000;
+        private const int c_SoftTemperatureLimitHasOccurred = 0x80000;
+
         internal PerformanceProperties RepopulateAndGetProperties()
         {
             if (!Utils.Hardware.isRunningRaspberryPi) { return this; }
 
-            if( Utils.Hardware.isRunningRaspberryPi5 == false)
-            {
-                var Task = ProcessRunner.GetProcessResultAsync(c_GrepCommand, "^dtoverlay=gpio-fan " + Utils.Hardware.ConfigPath);
-                Task.Wait();
+            Task<ProcessResult>[] Tasks;
 
+            if (Utils.Hardware.isRunningRaspberryPi5 == false)
+            {
+                Tasks = new Task<ProcessResult>[2];
+                Tasks[0] = ProcessRunner.GetProcessResultAsync(c_VCGenCommand, "get_throttled");
+                Tasks[1] = ProcessRunner.GetProcessResultAsync(c_GrepCommand, "^dtoverlay=gpio-fan " + Utils.Hardware.ConfigPath);
+            }
+            else
+            {
+                Tasks = new Task<ProcessResult>[1];
+                Tasks[0] = ProcessRunner.GetProcessResultAsync(c_VCGenCommand, "get_throttled");
+            }
+
+            Task.WaitAll(Tasks);
+
+            UndervoltageDetected = false;
+            ArmFrequencyCapped = false;
+            CurrentlyThrottled = false;
+            SoftTemperatureLimitActive = false;
+            UndervoltageHasOccurred = false;
+            ArmFrequencyCappingHasOccurred = false;
+            ThrottlingHasOccurred = false;
+            SoftTemperatureLimitHasOccurred = false;
+
+            if (Tasks[0].Result.Okay())
+            {
+                var SplitByX = Tasks[0].Result.GetOutput().Split(new char[] { 'x' });
+
+                if(SplitByX.Length > 1)
+                {
+                    var Result = SplitByX[1].Trim();
+
+                    int HexValue = int.Parse(Result, System.Globalization.NumberStyles.AllowHexSpecifier);
+
+                    if((HexValue & c_UndervoltageDetected) != 0)
+                    {
+                        UndervoltageDetected = true;
+                    }
+                    if ((HexValue & c_ArmFrequencyCapped) != 0)
+                    {
+                        ArmFrequencyCapped = true;
+                    }
+                    if ((HexValue & c_CurrentlyThrottled) != 0)
+                    {
+                        CurrentlyThrottled = true;
+                    }
+                    if ((HexValue & c_SoftTemperatureLimitActive) != 0)
+                    {
+                        SoftTemperatureLimitActive = true;
+                    }
+                    if ((HexValue & c_UndervoltageHasOccurred) != 0)
+                    {
+                        UndervoltageHasOccurred = true;
+                    }
+                    if ((HexValue & c_ArmFrequencyCappingHasOccurred) != 0)
+                    {
+                        ArmFrequencyCappingHasOccurred = true;
+                    }
+                    if ((HexValue & c_ThrottlingHasOccurred) != 0)
+                    {
+                        ThrottlingHasOccurred = true;
+                    }
+                    if ((HexValue & c_SoftTemperatureLimitHasOccurred) != 0)
+                    {
+                        SoftTemperatureLimitHasOccurred = true;
+                    }
+                }
+            }
+
+            if ( Utils.Hardware.isRunningRaspberryPi5 == false)
+            {
                 FanEnabled = false;
 
-                if ( Task.Result.Okay())
+                if (Tasks[1].Result.Okay())
                 {
-                    string Result = Task.Result.GetOutput();
+                    string Result = Tasks[1].Result.GetOutput();
 
                     if ( string.IsNullOrEmpty(Result) )
                     {
--- src/MultiPlug.Ext.RasPi.Config/Models/Components/Performance/PerformanceProperties.cs
+++ src/MultiPlug.Ext.RasPi.Config/Models/Components/Performance/PerformanceProperties.cs
@@ -13,5 +13,14 @@ namespace MultiPlug.Ext.RasPi.Config.Models.Components.Performance
         public int GPIOMin = 2;
         public int GPIOMax = 27;
 
+        public bool UndervoltageDetected { get; set; }
+        public bool ArmFrequencyCapped { get; set; }
+        public bool CurrentlyThrottled { get; set; }
+        public bool SoftTemperatureLimitActive { get; set; }
+        public bool UndervoltageHasOccurred { get; set; }
+        public bool ArmFrequencyCappingHasOccurred { get; set; }
+        public bool ThrottlingHasOccurred { get; set; }
+        public bool SoftTemperatureLimitHasOccurred { get; set; }
+
     }
 }
